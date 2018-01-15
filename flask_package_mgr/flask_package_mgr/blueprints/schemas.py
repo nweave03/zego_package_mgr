fdf56ebcfb1cfe6bd0f56434288d62dd0801b6d4
Nick Weaver <nickrweaver@gmail.com>
2018-01-14 22:37:44 -0600

    forgot to include schemas file

---
+++ flask_package_mgr/flask_package_mgr/blueprints/schemas.py
@@ -0,0 +1,33 @@
+from marshmallow import Schema, fields
+
+class TokenSchema(Schema):
+    username = fields.Str(
+                required=True,
+                error_messages={'required' : 'username is required'}
+                )
+    password = fields.Str(
+                required=True,
+                error_messages={'required' : 'password is required'}
+                )
+
+class PackagesGetSchema(Schema):
+    search = fields.Str()
+
+class PackagesPostSchema(Schema):
+    package_name = fields.Str(
+                required=True,
+                error_messages={'required' : 'package_name is required'}
+                )
+    tag = fields.Str(
+                required=True,
+                error_messages={'required' : 'tag is required'}
+                )
+
+class PackagesTitleGetSchema(Schema):
+    tag = fields.Str()
+
+class PackagesTitlePostSchema(Schema):
+    tag = fields.Str(
+                required=True,
+                error_messages={'required' : 'tag is required'}
+                )
